52cb6ec993ca3c78bf0fc331669601c6a5d35c31
Oleg Golev <ogolev@princeton.edu>
2023-03-01 10:00:05 -0500

    got the weights

--- load.py
+++ load.py
@@ -5,7 +5,11 @@ from Utils import load
 model = load.model('vgg16', 'lottery')((3, 32, 32), 10, False, False)
 device = torch.device('cpu')
 model.load_state_dict(torch.load('/scratch/network/ogolev/Results/data/singleshot/hp-rand-0.5/model.pt', map_location=device))
-i = 0
-for layer in model.children():
-    print(layer.state_dict()[str(i) + '.conv.weight'])
-    i += 1
+for i in range(16):
+    try:
+        conv_layer = model.layers[i].conv
+        weights = conv_layer.state_dict()['weight']
+        print(weights)
+    except:
+        # this is a MaxPool2D layer, so we don't care about this one?                                                                                                                        
+        continue
